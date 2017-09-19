1a93052884e1fd561159717e4407b73282e23f6f
Marcel Wege <mwege@byte5.de>
2017-09-19 13:26:53 +0200

    Initial Checkin

---
+++ siwecosscanner.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              byte5.de
+ * @since             0.0.1
+ * @package           Scanner
+ *
+ * @wordpress-plugin
+ * Plugin Name:       SIWECOS CMS Connector
+ * Plugin URI:        siwecos.de
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           0.1.2
+ * Author:            SIWECOS
+ * Author URI:        byte5.de
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       scanner
+ * Domain Path:       /languages
+ */
+
